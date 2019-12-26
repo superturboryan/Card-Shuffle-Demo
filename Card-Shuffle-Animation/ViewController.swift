c2edaaef63e28003cd2e7a9fbb52fcc81318f691
superturboryan <forsyth.r@gmail.com>
2019-12-26 12:45:53 -0500

    Initial Commit

---
+++ Card-Shuffle-Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Card-Shuffle-Animation
+//
+//  Created by Ryan David Forsyth on 2019-12-26.
+//  Copyright © 2019 Ryan David Forsyth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Card-Shuffle-AnimationTests/Card_Shuffle_AnimationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Card_Shuffle_AnimationTests.swift
+//  Card-Shuffle-AnimationTests
+//
+//  Created by Ryan David Forsyth on 2019-12-26.
+//  Copyright © 2019 Ryan David Forsyth. All rights reserved.
+//
+
+import XCTest
+@testable import Card_Shuffle_Animation
+
+class Card_Shuffle_AnimationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
